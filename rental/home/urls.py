f0fcba50507438665b1ba9422d318b8f6dd56516
safayetjamil647 <work.jamil4@gmail.com>
2023-07-17 21:26:27 +0600

    website part ready

--- rental/home/urls.py
+++ rental/home/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-
+app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
+    path('cars/', views.cars, name='cars'),
+    path('pricing/', views.pricings, name='pricings'),
+    path('contact/', views.contact, name='contact'),
+    path('cars/', views.car_list, name='car_list'),
 ]
--- rental/home/views.py
+++ rental/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from cars.models import Car
 def index(request):
     return render(request, 'home/index.html')
 
@@ -8,6 +9,13 @@ def about(request):
 def services(request):
     return render(request, 'home/services.html')
 
-def pricing(request):
+def pricings(request):
     return render(request, 'home/pricing.html')
 
+def cars(request):
+    return render(request, 'home/cars.html')
+def car_list(request):
+    cars = Car.objects.all()
+    return render(request, 'home/cars.html', {'cars': cars})
+def contact(request):
+    return render(request, 'home/contact.html')
